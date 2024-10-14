6e93032a3cb3f37bf4efb25ac3ee740200bc3761
DVDOSO <davidjiang0307@gmail.com>
2024-10-13 22:20:48 -0400

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,6 +2,20 @@ import styles from './Section.module.css';
 import ScrollAnimation from "react-animate-on-scroll";
 
 function Section(props) {
+    if(window.innerHeight > window.innerWidth){
+        return (
+            <section className={styles.section} style={{"--ACCENT_COLOR_1": props.color}}>
+                <ScrollAnimation animateIn='animate__animated animate__slideInUp'>
+                <article className={styles.text}>
+                    <h2 className={styles.title}>{props.title}</h2>
+                    <p className={styles.caption} style={{"max-width": '75%'}}>{props.text}</p>
+                    {props.buttons.map((button) => <a id='nowrap' className={styles.buttonLink} key={button.text} href={button.link}>{button.text}</a>)}
+                </article>
+                <img className={styles.figure} src={props.imgPath} alt={props.title} style={{"width": "max(" + props.width + ", 15rem)"}} />
+                </ScrollAnimation>
+            </section>
+        )
+    }
     if(props.order === 'left') {
         return (
             <section className={styles.section} style={{"--ACCENT_COLOR_1": props.color}}>
